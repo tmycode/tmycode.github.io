8797db5c13bd1daf6946ba0f0a9fa1fe4ed1b248
jie feng <jiefeng@jie-2.local>
2020-01-21 21:21:06 +1030

    a

---
+++ animation/main.js
@@ -0,0 +1,32 @@
+var tID; //we will use this variable to clear the setInterval()
+
+function stopAnimate() {
+  clearInterval(tID);
+} //end of stopAnimate()
+
+function animateScript() {
+
+  var position = 256; //start position for the image slicer
+  const interval = 100; //100 ms of interval for the setInterval()
+  const diff = 256; //diff as a variable for position offset
+  
+  tID = setInterval(() => {
+  
+    document.getElementById("image").style.backgroundPosition =
+      `-${position}px 0px`;
+    //we use the ES6 template literal to insert the variable "position"
+    
+    if (position < 1536) {
+      position = position + diff;
+    }
+    //we increment the position by 256 each time
+    else {
+      position = 256;
+    }
+    //reset the position to 256px, once position exceeds 1536px
+    
+  }, interval); //end of setInterval
+} //end of animateScript()
+
+document.getElementById("image").addEventListener("mouseover", animateScript);
+document.getElementById("image").addEventListener("mouseout", stopAnimate);
--- sw.js
+++ sw.js
@@ -3,14 +3,13 @@ self.addEventListener('fetch', function(e) {
   if(url.host.startsWith('localhost')) {
     return; // do default
   }
-  else if(url.pathname.startsWith("/lib/")) {
+  else if(url.pathname.includes("/lib/")) {
     let promised = caches.match(e.request).then(response =>{
       if(response) {
         return response;   
       }
       else {
-        let path = url.pathname.substr(4);// remove "/lib"
-        let h = `https://tmcodesdk.github.io${path}/index.js`;
+        let h = `https://tmycode.github.io${url.pathname}/index.js`;
         return fetch(new Request(h))
           .then(resp => {
             caches.open('v1').then(function(cache) {
